efc92525ca29446038ea7100942b5fc136bac434
oksana <sarkitovao@gmail.com>
2022-05-19 17:36:58 +0300

    add firstName, lastName, phoneNumber

--- server/models/category.js
+++ server/models/category.js
@@ -2,8 +2,8 @@ const sequelize = require('../db')
 const { DataTypes } = require('sequelize')
 
 const Category = sequelize.define('category', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
 })
 
 module.exports = Category
