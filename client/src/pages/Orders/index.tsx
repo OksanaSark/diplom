de1716f001168fbb73e0ff7553f782e8725392e8
oksana <sarkitovao@gmail.com>
2022-06-08 17:30:58 +0300

    fix styles

--- client/src/pages/Orders/index.tsx
+++ client/src/pages/Orders/index.tsx
@@ -15,34 +15,42 @@ export const Orders = observer(() => {
         }
     }, [])
 
+    const renderOrders = () => {
+        if (orderStore.orders.length) {
+            return orderStore.orders.map((order) => {
+                return (
+                    <div key={order.id} className='orderContainer'>
+                        <div className='orderHeader'>
+                            <p className='orderCode'>{Strings.orderCode} {order.id}</p>
+                            <p>{getDate(order.date)}</p>
+                        </div>
+                        <div className='productsContainer'>
+                            {order.products.map((product, index) => {
+                                return (
+                                    <div key={product.id} className='productContainer'>
+                                        <p className='productText'>{index + 1}. {product.name} - {product.count} {Strings.piece}</p>
+                                        <p className='productText'>{product.price}  &#8381;</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className='totalPriceContainer'>
+                            <p className='totalPrice'>{Strings.totalPrice}</p>
+                            <p className='totalPrice'>{order.totalPrice} &#8381;</p>
+                        </div>
+                    </div>
+                )
+            })
+
+            return <p>{Strings.emptyOrders}</p>
+        }
+    }
+
     return (
         <OrdersComponent>
             <p className='ordersTitle'>{Strings.orders}</p>
             <div className='ordersContainer'>
-                {orderStore.orders.map((order) => {
-                    return (
-                        <div key={order.id} className='orderContainer'>
-                            <div className='orderHeader'>
-                                <p className='orderCode'>{Strings.orderCode} {order.id}</p>
-                                <p>{getDate(order.date)}</p>
-                            </div>
-                            <div className='productsContainer'>
-                                {order.products.map((product, index) => {
-                                    return (
-                                        <div key={product.id} className='productContainer'>
-                                            <p className='productText'>{index + 1}. {product.name} - {product.count} {Strings.piece}</p>
-                                            <p className='productText'>{product.price}  &#8381;</p>
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                            <div className='totalPriceContainer'>
-                                <p className='totalPrice'>{Strings.totalPrice}</p>
-                                <p className='totalPrice'>{order.totalPrice} &#8381;</p>
-                            </div>
-                        </div>
-                    )
-                })}
+                {renderOrders()}
             </div>
         </OrdersComponent>
     )
--- client/src/pages/Orders/strings.tsx
+++ client/src/pages/Orders/strings.tsx
@@ -1,6 +1,7 @@
 export const Strings = {
     orders: 'Заказы',
     totalPrice: 'Общая сумма заказа:',
+    emptyOrders: 'Нет заказов :(',
     orderCode: 'Код заказа',
     piece: 'шт',
 }
