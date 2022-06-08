0ee9dd680dd5693d8527bdd2ca7f9bf3f9cde507
oksana <sarkitovao@gmail.com>
2022-06-08 13:58:16 +0300

    fix db.js

--- server/db.js
+++ server/db.js
@@ -5,6 +5,7 @@ module.exports = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
+        listen_addresses: '*',
         dialect: 'postgres',
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
